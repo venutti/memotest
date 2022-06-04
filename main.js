b551f65437cdff2850cb3d4063c59dccc860ee6a
venutti <lvenutti@fi.uba.ar>
2022-06-04 13:46:12 -0300

    agrego funcionalidad basica
    Se agregan los archivos fundamentales de HTML, CSS y JS.
    Se crea una estructura básica de HTML.
    Se crea un código de JS funcional al juego del memotest.

---
+++ main.js
@@ -0,0 +1,81 @@
+let cantidadParejas = 6;
+let $tablero = document.querySelector("#tablero");
+let distribucion = [0,0,1,1,2,2,3,3,4,4,5,5];
+let tarjetasEnEspera = [];
+
+mezclarArray(distribucion);
+
+comenzarJuego();
+
+function comenzarJuego() {
+    habilitarSeleccionTarjetas();
+}
+
+function evaluarSeleccion() {
+    if(tarjetasEnEspera.length === 2) {
+        $tarjeta1 = tarjetasEnEspera[0];
+        $tarjeta2 = tarjetasEnEspera[1];
+
+        deshabilitarSeleccionTarjetas();
+        if(obtenerValorTarjeta(tarjetasEnEspera[0]) === obtenerValorTarjeta(tarjetasEnEspera[1])) {
+            cantidadParejas--;
+            evaluarJuego();
+            console.log("Son iguales!");
+        }else {
+            console.log("Ups, te equivocaste!");
+            setTimeout(function() {
+                voltearAbajo($tarjeta1);
+                voltearAbajo($tarjeta2);
+            }, 1000);
+
+        }
+        setTimeout(habilitarSeleccionTarjetas, 1000);
+        tarjetasEnEspera = [];
+    }
+}
+
+function evaluarJuego() {
+    if(cantidadParejas === 0) {
+        document.querySelector("#alerta-estado").textContent = "Ganaste!";
+    }
+}
+
+function habilitarSeleccionTarjetas() {
+    document.querySelectorAll("#tablero .tarjeta").forEach(function($tarjeta) {
+        $tarjeta.onclick = function() {
+            if(!$tarjeta.classList.contains("seleccionado")) {
+                $tarjeta.classList.add("seleccionado");
+                voltearArriba($tarjeta);
+                tarjetasEnEspera.push($tarjeta);
+                evaluarSeleccion();
+            }
+        };
+    });
+}
+
+function deshabilitarSeleccionTarjetas() {
+    document.querySelectorAll("#tablero .tarjeta").forEach(function($tarjeta) {
+        $tarjeta.onclick = function() {
+
+        }
+    });
+}
+
+function voltearArriba($tarjeta) {
+    $tarjeta.classList.remove("seleccionado");
+    $tarjeta.querySelector("p").textContent = distribucion[Number($tarjeta.id)];
+}
+
+function voltearAbajo($tarjeta) {
+    $tarjeta.querySelector("p").textContent = "?";
+}
+
+function mezclarArray(array){
+    array.sort(function() {
+        return Math.random() - 0.5;
+    });
+}
+
+function obtenerValorTarjeta($tarjeta) {
+    return $tarjeta.querySelector("p").textContent;
+}
